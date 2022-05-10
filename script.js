921f839a25e9cd08f1fe76dc1b2ee7eca63d873e
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-05-09 23:32:00 -0300

    Requisito 9 concluído

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // Cria elemento HTML
 function criaDivElement(coluna) {
-
-  for(let index = 0; index < coluna; index +=1) {
-    let novaDiv = document.createElement('div');
+  for (let index = 0; index < coluna; index += 1) {
+    const novaDiv = document.createElement('div');
     novaDiv.className = 'pixel';
-    let main = document.getElementById('pixel-board');
-  
-    for (let index2 = 0; index2 < coluna - 1; index2 +=1) {
-      let novaColuna = document.createElement('div');
+    const main = document.getElementById('pixel-board');
+    for (let index2 = 0; index2 < coluna - 1; index2 += 1) {
+      const novaColuna = document.createElement('div');
       novaColuna.className = 'pixel';
-      let paiDaDiv = document.getElementById('pixel-board');
+      const paiDaDiv = document.getElementById('pixel-board');
     paiDaDiv.appendChild(novaColuna);
     }
     main.appendChild(novaDiv);
@@ -19,34 +17,35 @@ criaDivElement(5);
 
 // Requisito 7:
 const acessaCor = document.querySelectorAll('.color');
-for (let index = 0; index < acessaCor.length; index +=1)
+for (let index = 0; index < acessaCor.length; index += 1) {
   acessaCor[index].addEventListener('click', selecionaCor);
-
+}
 function selecionaCor(event) {
-  const selected =  document.querySelector('.selected');
+  const selected = document.querySelector('.selected');
   selected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
 // Requisito 8:
 const pixels = document.querySelectorAll('.pixel');
-for (let percorrePixel = 0; percorrePixel < pixels.length; percorrePixel +=1) {
+for (let percorrePixel = 0; percorrePixel < pixels.length; percorrePixel += 1) {
   pixels[percorrePixel].addEventListener('click', preenchePixel);
 }
 
 function preenchePixel(event) {
   const classeSelected = document.querySelector('.selected');
-  const importandoCor = window.getComputedStyle(classeSelected, null).getPropertyValue('background-color');
-  event.target.style.backgroundColor = importandoCor;
+  const importaCor = window.getComputedStyle(classeSelected).getPropertyValue('background-color');
+  event.target.style.backgroundColor = importaCor;
 }
 
 // Requisito 9:
-// let clearBoard = document.getElementById('clear-board');
-// clearBoard.addEventListener('click', reset);
-
-// function reset(){
-//   let pixelBoard = document.getElementsByClassName('pixel');
-//   for (let index = 0; index < pixels.length; index += 1) {
-//     pixelBoard[index].style.backgroundColor = 'white';
-//   }
-// }
+function reset() {
+  const clearBoard = document.getElementById('clear-board');
+  clearBoard.addEventListener('click', function() {
+    document.location.reload(true);
+  });
+}
+reset();
+// Fontes:
+// https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+// Mentoria Summer Trybe com Bruno Feu
