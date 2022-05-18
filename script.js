b018d40ff8cbc88df1524dcbd034f7b0df9f7e9a
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-05-18 12:45:40 -0300

    Requisito 10 e 11 concluído

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 // Cria elemento HTML
 function criaDivElement(coluna) {
   for (let index = 0; index < coluna; index += 1) {
-    const novaDiv = document.createElement('div');
-    novaDiv.className = 'pixel';
+    const novaSection = document.createElement('section');
+    novaSection.className = 'pixelline';
     const main = document.getElementById('pixel-board');
-    for (let index2 = 0; index2 < coluna - 1; index2 += 1) {
+    main.appendChild(novaSection);
+    for (let index2 = 0; index2 < coluna; index2 += 1) {
       const novaColuna = document.createElement('div');
       novaColuna.className = 'pixel';
-      const paiDaDiv = document.getElementById('pixel-board');
-    paiDaDiv.appendChild(novaColuna);
+      novaSection.appendChild(novaColuna);
     }
-    main.appendChild(novaDiv);
   }
 }
 criaDivElement(5);
 
 // Requisito 7:
-const acessaCor = document.querySelectorAll('.color');
-for (let index = 0; index < acessaCor.length; index += 1) {
-  acessaCor[index].addEventListener('click', selecionaCor);
+function selecCor() {
+  const acessaCor = document.querySelectorAll('.color');
+  for (let index = 0; index < acessaCor.length; index += 1) {
+    acessaCor[index].addEventListener('click', selecionaCor);
+  }
+  function selecionaCor(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
 }
-function selecionaCor(event) {
-  const selected = document.querySelector('.selected');
-  selected.classList.remove('selected');
-  event.target.classList.add('selected');
-}
+selecCor();
 
 // Requisito 8:
-const pixels = document.querySelectorAll('.pixel');
-for (let percorrePixel = 0; percorrePixel < pixels.length; percorrePixel += 1) {
-  pixels[percorrePixel].addEventListener('click', preenchePixel);
-}
+function preencheCor() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let percorrePixel = 0; percorrePixel < pixels.length; percorrePixel += 1) {
+    pixels[percorrePixel].addEventListener('click', preenchePixel);
+  }
 
-function preenchePixel(event) {
-  const classeSelected = document.querySelector('.selected');
-  const importaCor = window.getComputedStyle(classeSelected).getPropertyValue('background-color');
-  event.target.style.backgroundColor = importaCor;
+  function preenchePixel(event) {
+    const classeSelected = document.querySelector('.selected');
+    const importaCor = window.getComputedStyle(classeSelected).getPropertyValue('background-color');
+    event.target.style.backgroundColor = importaCor;
+  }
 }
+preencheCor();
 
 // Requisito 9:
 function reset() {
@@ -46,6 +51,32 @@ function reset() {
   });
 }
 reset();
-// Fontes:
+
+// Requisito 10:
+function user() {
+  const button = document.getElementById('generate-board');
+  const input = document.getElementById('board-size');
+  button.addEventListener('click', () => {
+    const quadroPixelss = document.querySelector('#pixel-board');
+      if (input.value === '') {
+        return alert('Board inválido!');
+      }
+      quadroPixelss.textContent = '';
+      if (input.value < 5) {
+        input.value = 5;
+      } 
+      if (input.value > 50) {
+        input.value = 50;
+      }
+      criaDivElement(input.value);
+      selecCor();
+      preencheCor();
+      reset();
+  });
+}
+user();
+
+  // Fontes:
 // https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 // Mentoria Summer Trybe com Bruno Feu
+
